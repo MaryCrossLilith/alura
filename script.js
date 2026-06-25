93110ffe55d33ab89977de0af0dd3d5d886857f1
Maria Eduarda Hurla <maria.hurla.oliveira@escola.pr.gov.br>
2026-06-25 20:49:43 -0300

    js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let btnAjuda = document.querySelector(".botao-ajuda");
 let btnFechar = document.querySelector(".botao-ajuda");
-let modal = document.querySelector(".botao-ajuda");
+let modal = document.querySelector(".modal-fundo");
+
+btnAjuda.addEventListener("click", abreModal);
+btnFechar.addEventListener("click", fechaModal);
